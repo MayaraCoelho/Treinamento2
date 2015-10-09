1bdafeeb60bfc9784e866464569a9e27c8852f10
linharesh <linhares.h@gmail.com>
2015-10-09 18:52:44 -0300

    arrumando pastas e arquivos

--- Wall Invest/Manager.swift
+++ Wall Invest/Manager.swift
@@ -2,7 +2,7 @@
 //  Manager.swift
 //  Wall Invest
 //
-//  Created by Henrique do Prado Linhares on 02/10/15.
+//  Created by Henrique do Prado Linhares on 09/10/15.
 //  Copyright © 2015 Henrique do Prado Linhares. All rights reserved.
 //
 
