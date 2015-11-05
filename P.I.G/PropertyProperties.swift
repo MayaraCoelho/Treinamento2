4b3b6652ddf72e347f611192798554ed1616ebed
mcseves <mariacarolina@MacBook-Pro-de-Maria-Carolina.local>
2015-11-05 14:30:07 -0200

    CORRIGI AGORA <3

--- P.I.G/PropertyProperties.swift
+++ P.I.G/PropertyProperties.swift
@@ -15,7 +15,7 @@ class PropertyProperties{
         
         self.properties = [Property]()
         
-        let p = Property(pName: "Casa", pValue: 100000, pReturningMinimumValue: 800, pReturningMaximumValue: 1187)
+        let p = Property(pName: "Casa", pValue: 100000, pReturningMinimumValue: 800, pReturningMaximumValue: 1187, pImage: "")
         properties.append(p)
     }
 
