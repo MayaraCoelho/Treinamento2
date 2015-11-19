f0ff228db3100fb591e537ae02d363f90e0238cf
mcseves <mariacarolina@MacBook-Pro-de-Maria-Carolina.local>
2015-11-19 16:30:22 -0200

    adicionei localizable strings e coloquei a traducao para todas as empresas, inv de baixo risco e propriedades

--- P.I.G/EnterpriseProperties.swift
+++ P.I.G/EnterpriseProperties.swift
@@ -17,44 +17,44 @@ class EnterpriseProperties{
     init(){
         self.enterprises = [Enterprise]()
         
-        let ent1desc = "Founded in Saudi Arabia, 1931, is the largest oil company in the world, responsible for the sale of about 400 barrels of oil a day to the United States."
-        let ent1 = Enterprise(pId: 1, pName: "Everest Oil", pDescription: ent1desc, pImageName: "EverestOil.png")
+        let ent1desc = NSLocalizedString("descE1",comment:"")
+        let ent1 = Enterprise(pId: 1, pName: NSLocalizedString("nameE1",comment:""), pDescription: ent1desc, pImageName: "EverestOil.png")
         self.enterprises.append(ent1)
         
-        let ent2desc = "Founded in 1980 by the student Frank Roy, acts creating powerful systems that changed the daily life of our society."
-        let ent2 = Enterprise(pId: 2, pName: "Bug Software Inc", pDescription: ent2desc, pImageName: "BugSoftwareInc.png")
+        let ent2desc = NSLocalizedString("descE2",comment:"")
+        let ent2 = Enterprise(pId: 2, pName: NSLocalizedString("nameE2",comment:""), pDescription: ent2desc, pImageName: "BugSoftwareInc.png")
         self.enterprises.append(ent2)
         
-        let ent3desc = "Brazilian corporation founded by the junction of the two largest food companies."
-        let ent3 = Enterprise(pId: 3, pName: "Bacompany House", pDescription: ent3desc, pImageName: "BacompanyHouse.png")
+        let ent3desc = NSLocalizedString("descE3",comment:"")
+        let ent3 = Enterprise(pId: 3, pName: NSLocalizedString("nameE3",comment:""), pDescription: ent3desc, pImageName: "BacompanyHouse.png")
         self.enterprises.append(ent3)
         
-        let ent4desc = "The French designer Amélie DeLarge created a small clothing store that has expanded and today has become a worldwide fashion icon."
-        let ent4 = Enterprise(pId: 4, pName: "Mr Always Young", pDescription: ent4desc, pImageName: "MrAlwaysYoung.png")
+        let ent4desc = NSLocalizedString("descE4",comment:"")
+        let ent4 = Enterprise(pId: 4, pName: NSLocalizedString("nameE4",comment:""), pDescription: ent4desc, pImageName: "MrAlwaysYoung.png")
         self.enterprises.append(ent4)
         
-        let ent5desc = "Company founded by three students from the University of Mexico specializes in developing highly regarded games as Nachos Locos."
-        let ent5 = Enterprise(pId: 5, pName: "El Juego Zone", pDescription: ent5desc, pImageName: "ElJuegoZone.png")
+        let ent5desc = NSLocalizedString("descE5",comment:"")
+        let ent5 = Enterprise(pId: 5, pName: NSLocalizedString("nameE5",comment:""), pDescription: ent5desc, pImageName: "ElJuegoZone.png")
         self.enterprises.append(ent5)
         
-        let ent6desc = "Media company that reaches the height of its success in 2005, due to the launch of a tabloid in Russia."
-        let ent6 = Enterprise(pId: 6, pName: "Time News Central", pDescription: ent6desc, pImageName: "TimeNewsCentral.png")
+        let ent6desc = NSLocalizedString("descE6",comment:"")
+        let ent6 = Enterprise(pId: 6, pName: NSLocalizedString("nameE6",comment:""), pDescription: ent6desc, pImageName: "TimeNewsCentral.png")
         self.enterprises.append(ent6)
         
-        let ent7desc = "Founded in 1910 by the couple Barney and Clara, is considered the largest bank for investment from all over Europe."
-        let ent7 = Enterprise(pId: 7, pName: "B&C", pDescription: ent7desc, pImageName: "B&C.png")
+        let ent7desc = NSLocalizedString("descE7",comment:"")
+        let ent7 = Enterprise(pId: 7, pName: NSLocalizedString("nameE7",comment:""), pDescription: ent7desc, pImageName: "B&C.png")
         self.enterprises.append(ent7)
         
-        let ent8desc = "It was founded by engineer Philip Holland in England, and is responsible for the largest buildings of the contemporary world."
-        let ent8 = Enterprise(pId: 8, pName: "Philip Holland Buildings", pDescription: ent8desc, pImageName: "PhilipHollandBuildings.png")
+        let ent8desc = NSLocalizedString("descE8",comment:"")
+        let ent8 = Enterprise(pId: 8, pName: NSLocalizedString("nameE8",comment:""), pDescription: ent8desc, pImageName: "PhilipHollandBuildings.png")
         self.enterprises.append(ent8)
         
-        let ent9desc = "Founded in Argentina in 1990, manufactures aircraft of all sizes and are famous for their excellent performance."
-        let ent9 = Enterprise(pId: 9, pName: "Dumont Aircrafts", pDescription: ent9desc, pImageName: "DumontAircrafts.png")
+        let ent9desc = NSLocalizedString("descE9",comment:"")
+        let ent9 = Enterprise(pId: 9, pName: NSLocalizedString("nameE9",comment:""), pDescription: ent9desc, pImageName: "DumontAircrafts.png")
         self.enterprises.append(ent9)
         
-        let ent10desc = "Startup created by retired postman George, it offers an unique service that takes your order anywhere in the world in 24 hours."
-        let ent10 = Enterprise(pId: 10, pName: "Fastex SA", pDescription: ent10desc, pImageName: "FastexSA.png")
+        let ent10desc = NSLocalizedString("descE10",comment:"")
+        let ent10 = Enterprise(pId: 10, pName: NSLocalizedString("nameE10",comment:""), pDescription: ent10desc, pImageName: "FastexSA.png")
         self.enterprises.append(ent10)
         
         
--- P.I.G/PropertyProperties.swift
+++ P.I.G/PropertyProperties.swift
@@ -15,25 +15,25 @@ class PropertyProperties{
         
         self.properties = [Property]()
         
-        let prop1 = Property(pID:7,pName: "Barraca de Fruta", pValue: 700, pReturningValue:10 , pImage: "fruitIcon.png")
+        let prop1 = Property(pID:7,pName: NSLocalizedString( "Barraca de Fruta",comment:""), pValue: 700, pReturningValue:10 , pImage: "fruitIcon.png")
         
-        let prop2 = Property(pID:6,pName: "Barraca de Pipoca", pValue: 1000, pReturningValue: 10, pImage: "popcornIcon.png")
+        let prop2 = Property(pID:6,pName: NSLocalizedString("Barraca de Pipoca",comment:"") , pValue: 1000, pReturningValue: 10, pImage: "popcornIcon.png")
         
-        let prop3 = Property(pID:8,pName: "Motorista de Transporte Alternativo", pValue: 5300, pReturningValue: 10,  pImage: "taxiIcon.png")
+        let prop3 = Property(pID:8,pName: NSLocalizedString("Motorista de Transporte Alternativo",comment:"") , pValue: 5300, pReturningValue: 10,  pImage: "taxiIcon.png")
         
-        let prop4 = Property(pID:1, pName: "Terreno", pValue: 20000, pReturningValue:10, pImage: "landIcon.png")
+        let prop4 = Property(pID:1, pName: NSLocalizedString("Terreno",comment:""), pValue: 20000, pReturningValue:10, pImage: "landIcon.png")
         
-        let prop5 = Property(pID:2,pName: "Apartamento", pValue: 50000, pReturningValue:10, pImage: "buildingIcon.png")
+        let prop5 = Property(pID:2,pName: NSLocalizedString("Apartamento",comment:""), pValue: 50000, pReturningValue:10, pImage: "buildingIcon.png")
         
-        let prop6 = Property(pID:3,pName: "Casa", pValue: 100000, pReturningValue: 800, pImage: "houseIcon.png")
+        let prop6 = Property(pID:3,pName: NSLocalizedString("Casa",comment:"") , pValue: 100000, pReturningValue: 800, pImage: "houseIcon.png")
         
-        let prop7 = Property(pID:9,pName: "Hamburgueria na Zona Sul", pValue: 120000, pReturningValue: 10, pImage: "burgerIcon.png")
+        let prop7 = Property(pID:9,pName: NSLocalizedString("Hamburgueria na Zona Sul",comment:""), pValue: 120000, pReturningValue: 10, pImage: "burgerIcon.png")
         
-        let prop8 = Property(pID:10,pName: "Rede com 5 restaurantes", pValue: 1500000, pReturningValue:10, pImage: "franchiseIcon.png")
+        let prop8 = Property(pID:10,pName: NSLocalizedString("Rede com 5 restaurantes",comment:""), pValue: 1500000, pReturningValue:10, pImage: "franchiseIcon.png")
         
-        let prop9 = Property(pID:4,pName: "Condominio Residencial", pValue: 1000000, pReturningValue: 10, pImage: "jointIcon.png")
+        let prop9 = Property(pID:4,pName: NSLocalizedString("Condominio Residencial",comment:""), pValue: 1000000, pReturningValue: 10, pImage: "jointIcon.png")
         
-        let prop10 = Property(pID:5,pName: "Shopping Center", pValue: 1000000000, pReturningValue:10, pImage: "shopIcon.png")
+        let prop10 = Property(pID:5,pName: NSLocalizedString("Shopping Center",comment:""), pValue: 1000000000, pReturningValue:10, pImage: "shopIcon.png")
 
         
         properties.append(prop1)
