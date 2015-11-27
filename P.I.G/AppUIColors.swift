53e0c1c987b976e68108be851bf6214a5119161c
linharesh <linhares.h@gmail.com>
2015-11-27 13:09:23 -0200

    varias modificacoes no visual do app

--- P.I.G/AppUIColors.swift
+++ P.I.G/AppUIColors.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 class AppUIColors{
-    let myGreenColor = UIColor(red: 97/255.0, green: 178/255.0, blue: 47/255.0, alpha: 1.0)
+    let myGreenColor = UIColor(red: 149/255.0, green: 198/255.0, blue: 117/255.0, alpha: 1.0)
     let myRedColor = UIColor(red: 207/255.0, green: 93/255.0, blue: 93/255.0, alpha: 1.0)
 }
